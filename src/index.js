904b1fc3b0bed255b05d913fe12ece33b4025208
nour mamdouh <154377982+lilpandaz69@users.noreply.github.com>
2025-05-19 17:58:11 +0300

    index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import initScrollReveal from "./scripts/scrollReveal.js";
 import initTiltEffect from "./scripts/tiltAnimation.js";
-import { targetElements, defaultProps } from "./src/data/scrollRevealConfig.js";
+// من src/scripts/index.js إلى src/data/scrollRevealConfig.js
+import { targetElements, defaultProps } from "../data/scrollRevealConfig.js";
+
 
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
