2cb16b25aa50e3a2128de6c1c11f18cc017d02cc
nour mamdouh <nourmamdouh54@gmai.com>
2025-05-19 18:10:46 +0300

    change

--- src/scripts/tiltAnimation.js
+++ src/scripts/tiltAnimation.js
@@ -1,6 +1,13 @@
-import VanillaTilt from "vanilla-tilt";
-
 export default function initTiltAnimation() {
   const elements = document.querySelectorAll(".js-tilt");
-  VanillaTilt.init(elements);
+  if (window.VanillaTilt) {
+    VanillaTilt.init(elements, {
+      max: 25,
+      speed: 400,
+      glare: true,
+      "max-glare": 0.5,
+    });
+  } else {
+    console.error("VanillaTilt is not loaded");
+  }
 }
